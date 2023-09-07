dd8766d65842b01290646e4301bccc6425222465
listyantidewi1 <listyantidewi@gmail.com>
2023-09-07 11:38:31 +0700

    add birthdays.db, imports

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,13 @@
-# import aplikasi flask untuk dipakai di web kita
-from flask import Flask
+# import aplikasi flask, os, flash, jsonify, redirect, dan render_template untuk dipakai di web kita
+import os
+
+# import SQL utk akses database
+from cs50 import SQL
+# import flash utk notifikasi pada web
+# import jsonify utk memformat data
+# import redirect dan render_template untuk berpindah halaman web
+# import request dan session untuk mengakses data
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
 # mengatur nama aplikasi
 app = Flask(__name__)
